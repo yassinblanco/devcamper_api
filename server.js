738f28f303720358842942d20c75f481d46c5836
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-01-17 21:56:51 +0100

    Creating routes

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/api/v1/bootcamps", (req, res) => {
+	res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+	res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+	res.status(200).json({ success: true, msg: `Create new bootcamp` });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
