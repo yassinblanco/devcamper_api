061f6e514837019256d7dc8ddaa12008369720e7
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-01-18 21:33:49 +0100

    Using the express router

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/api/v1/bootcamps", (req, res) => {
-	res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-	res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-	res.status(200).json({ success: true, msg: `Create new bootcamp` });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-	res
-		.status(200)
-		.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-	res
-		.status(200)
-		.json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
 
 app.listen(
 	PORT,
