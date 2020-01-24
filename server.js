7af38740aae07773d2f3ee14e83528ab00285c7d
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-01-24 20:50:48 +0100

    Error handler middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === "development") {
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
