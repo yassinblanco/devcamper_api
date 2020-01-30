2a177d74810ad469a25dc449840920d86a7c219a
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-01-30 20:32:10 +0100

    Pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
 	const reqQuery = { ...req.query };
 
-	const removeFields = ["select", "sort"];
+	const removeFields = ["select", "sort", "page", "limit"];
 
 	removeFields.forEach(elm => delete reqQuery[elm]);
 
@@ -35,10 +35,40 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 		query = query.sort("-createdAt");
 	}
 
+	//Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Bootcamp.countDocuments();
+
+	query = query.skip(startIndex).limit(limit);
+
 	const bootcamps = await query;
-	res
-		.status(200)
-		.json({ success: true, count: bootcamps.length, data: bootcamps });
+
+	//Pagination result
+	const pagination = {};
+
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit
+		};
+	}
+
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit
+		};
+	}
+
+	res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		pagination,
+		data: bootcamps
+	});
 });
 
 //@desc  Get single bootcamp
