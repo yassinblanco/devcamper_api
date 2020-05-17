a8120b6f979bdda2f952c12155cc034da8054966
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-05-17 14:44:33 +0000

    add auth contoller and auth route

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ connectDB();
 //Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -35,6 +36,7 @@ if (process.env.NODE_ENV === "development") {
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
